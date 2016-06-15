5ea2517eecf0625dffb03bba3de44c90fda4569b
Yeongtaek, Hong <mozodev@users.noreply.github.com>
2016-06-15 14:25:45 +0900

    remove vagrant file, fix for guest addition, cleanup

--- scripts/virtualbox.sh
+++ scripts/virtualbox.sh
@@ -1,6 +1,6 @@
 # Install the VirtualBox guest additions
 VBOX_ISO=VBoxGuestAdditions.iso
-mount -o loop /home/vagrant/$VBOX_ISO /mnt
+mount -o loop $VBOX_ISO /mnt
 yes|sh /mnt/VBoxLinuxAdditions.run
 umount /mnt
 
